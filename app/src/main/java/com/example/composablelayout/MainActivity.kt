75de2e45b7a4188e27e6a67ad3c2dc614bfa19e0
RamaRamdani03 <ramaramdani449@gamil.com>
2024-10-18 19:24:11 +0700

    Perbarui composable Image untuk memuat foto profil dengan deskripsi konten

--- app/src/main/java/com/example/composablelayout/MainActivity.kt
+++ app/src/main/java/com/example/composablelayout/MainActivity.kt
@@ -5,16 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -106,7 +109,6 @@ fun BasicCompose(
         Image(
             painter = painterResource(id = R.drawable.sayaaa),
             contentDescription = null,
-            modifier = Modifier.size(200.dp)
         )
     }
 }
