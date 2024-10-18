61eebae5d827c1012977d7cf4304752d15a1a4d7
RamaRamdani03 <ramaramdani449@gamil.com>
2024-10-18 18:28:13 +0700

    Menambahkan Teks Login ke BasicCompose dengan gaya kustom

--- app/src/main/java/com/example/composablelayout/MainActivity.kt
+++ app/src/main/java/com/example/composablelayout/MainActivity.kt
@@ -13,8 +13,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composablelayout.ui.theme.ComposableLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,9 +46,18 @@ fun BasicCompose(
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top,
-        modifier = modifier.fillMaxSize().padding(top = 32.dp)
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 32.dp)
     ){
-
+        Text(
+            text = "Login",
+            style = TextStyle(
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Gray
+            )
+        )
     }
 }
 
